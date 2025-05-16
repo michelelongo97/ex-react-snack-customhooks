6c920dd5470fe113b019e8a589b63b826429ac85
michelelongo97 <m.longo7x@gmail.com>
2025-05-16 13:05:11 +0200

    feat: add snack 1

--- src/useSwitch.jsx
+++ src/useSwitch.jsx
@@ -1,1 +1,9 @@
-export default function useSwitch() {}
+import { useState } from "react";
+
+export default function useSwitch(initialValue = false) {
+  const [isOn, setIsOn] = useState(initialValue);
+  const toggle = () => {
+    setIsOn((curr) => !curr);
+  };
+  return [isOn, toggle];
+}
