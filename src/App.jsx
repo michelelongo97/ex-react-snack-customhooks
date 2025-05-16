daecfae9c02392d49fc429ebddc6b28f1cf83803
michelelongo97 <m.longo7x@gmail.com>
2025-05-16 13:13:27 +0200

    feat: add snack 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import useSwitch from "./useSwitch";
+import useDate from "./useDate";
 
 function App() {
-  const [isOn, toggle] = useSwitch();
+  //const [isOn, toggle] = useSwitch();
+
+  //return (
+  //  <div>
+  //    <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+  //    <button onClick={toggle}>Cambia Stato</button>
+  //  </div>
+  //);
+
+  const currentDate = useDate();
 
   return (
     <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
+      <h1>Data e ora attuali:</h1>
+      <p>{currentDate.toLocaleString()}</p>
     </div>
   );
 }
